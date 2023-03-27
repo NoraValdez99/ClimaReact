f63f0ecbb7518363a5e57c7043201c1e8abcc65b
Nora A. Valdez <39319331+NoraValdez99@users.noreply.github.com>
2023-03-26 23:07:16 -0600

    Add files via upload

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,26 @@
+import useClima from '../hooks/useClima'
+import { Formulario } from './Formulario'
+import { Resultado } from './Resultado'
+import { Loading } from './Loading'
+
+export const AppClima = () => {
+
+    const {resultado, cargando, noResultado} = useClima()
+
+
+  return (
+        <>
+            <main className='dos-columnas'>
+                <Formulario />
+
+                {
+                    cargando ? <Loading/> :                
+                    resultado?.name ? <Resultado /> :
+                    noResultado ? <p>{noResultado}</p> :
+                    <p>El clima se va a mostrar aquí</p>
+                }
+            </main>
+        
+        </>
+  )
+}
